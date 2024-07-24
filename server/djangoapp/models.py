ee8a4a39e6b70cc6bd3928e62239ceb855dae10c
Seven <ahmadaliev.sd@gmail.com>
2024-07-24 12:12:03 +0900

    car models/url-paths/views created

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,20 +1,26 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    established_year = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
 # - Name
@@ -23,3 +29,29 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    HATCHBACK = 'Hatchback'
+    CONVERTIBLE = 'Convertible'
+    COUPE = 'Coupe'
+    TRUCK = 'Truck'
+    
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (HATCHBACK, 'Hatchback'),
+        (CONVERTIBLE, 'Convertible'),
+        (COUPE, 'Coupe'),
+        (TRUCK, 'Truck')
+    ]
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models') # Many-to-one relationship
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES, default='Sedan')
+    year = models.IntegerField(default=2024)
+    color = models.CharField(max_length=50, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_user, name='logout'),
 
+    # path for get_cars view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
