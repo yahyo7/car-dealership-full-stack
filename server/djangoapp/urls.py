1be6ce39c1107e7ebfaaa80fce8b9c012fb3a2cd
Yakhyokhon(Seven) <ahmadaliev.sd@gmail.com>
2024-07-24 08:35:49 -0400

    fixed some error

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-app_name = 'djangoapp'
+app_name = 'djangoapp' 
 urlpatterns = [
     # # path for registration
     path(route='register', view=views.registration, name='register'),
@@ -20,8 +20,8 @@ urlpatterns = [
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # path for dealer reviews view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='get_dealer_reviews'),
     # path for add a review view
-        path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
